f15021b7fd06f8fe41b945a0feaffc9376f136c5
Nand72820 <duhgfhfdghg@gmail.com>
2024-10-14 06:09:30 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ cat <<EOL > .devcontainer/devcontainer.json
             "version": "latest"
         }
     },
-    "postStartCommand": "REPO_NAME=\$(basename \$(git rev-parse --show-toplevel)); python3 /workspaces/\$REPO_NAME/m.py",
+    "postStartCommand": "REPO_NAME=\$(basename \$(git rev-parse --show-toplevel)); python3 /workspaces/\$REPO_NAME/nand.py",
     "customizations": {
         "vscode": {
             "settings": {
